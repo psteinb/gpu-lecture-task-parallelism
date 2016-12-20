2cdde36384ed8b50b2a6478a1a9681270e860274
Peter Steinbach <steinbac@mpi-cbg.de>
2016-12-20 12:13:37 +0100

    added mean, sd functions

--- competition_openmp/competition.cu
+++ competition_openmp/competition.cu
@@ -11,3 +11,21 @@ TEST_CASE_METHOD(competition_fixture, "fixture_works" ) {
  
 }
 
+TEST_CASE_METHOD(competition_fixture, "mean_is_42" ) {
+
+  REQUIRE(std::abs(mean(small.begin(), small.end()) - 42.) < 1);
+  REQUIRE(std::abs(mean(medium.begin(), medium.end()) - 42.) < 1);
+  REQUIRE(std::abs(mean(large.begin(), large.end()) - 42.) < 1);
+   
+}
+
+TEST_CASE_METHOD(competition_fixture, "sd_is_around_10" ) {
+
+  REQUIRE(std::abs(sd(small.begin(), small.end()) - 10.) < 1);
+  REQUIRE(std::abs(sd(medium.begin(), medium.end()) - 10.) < 1);
+  REQUIRE(std::abs(sd(large.begin(), large.end()) - 10.) < 1);
+   
+}
+
+
+
