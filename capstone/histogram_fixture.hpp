7faba43bc755af23e4289bf05297116a5f0789d7
Peter Steinbach <steinbac@mpi-cbg.de>
2017-12-12 11:01:21 +0100

    fixed compilation bugs

--- capstone/histogram_fixture.hpp
+++ capstone/histogram_fixture.hpp
@@ -12,12 +12,12 @@ struct histogram_fixture {
   int frame_size;
   std::vector<std::uint8_t> histo;
 
-  array_fixture() :
+  histogram_fixture() :
     frame_size(1024*1024),
     histo(n_mega_elements*1024*1024,0){
 
-    std::random_device rd{20171212};
-    std::mt19937 gen{rd()};
+    //    std::random_device rd{20171212};
+    std::mt19937 gen{20171212};
 
     std::normal_distribution<> d{64,20};
 
