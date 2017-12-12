9638b426491496c3a2a5131f3898cf14d47a9465
Peter Steinbach <steinbac@mpi-cbg.de>
2017-12-12 12:32:53 +0100

    upgraded test suite

--- capstone/histogram_fixture.hpp
+++ capstone/histogram_fixture.hpp
@@ -10,18 +10,20 @@ template <int n_mega_elements = 16>
 struct histogram_fixture {
 
   int frame_size;
-  std::vector<std::uint8_t> histo;
+  std::vector<std::uint8_t> image;
+  std::vector<std::uint32_t> histo;
 
   histogram_fixture() :
     frame_size(1024*1024),
-    histo(n_mega_elements*1024*1024,0){
+    image(n_mega_elements*1024*1024,0),
+    histo(256,0){
 
     //    std::random_device rd{20171212};
     std::mt19937 gen{20171212};
 
     std::normal_distribution<> d{64,20};
 
-    for( std::uint8_t& el : histo )
+    for( std::uint8_t& el : image )
       el = std::round(d(gen));
   }
 
--- capstone/test_histogram.cu
+++ capstone/test_histogram.cu
@@ -3,9 +3,18 @@
 
 #include "helper_cuda.h"
 
+#include <iterator>
 
 TEST_CASE_METHOD(histogram_fixture<16> , "fixture_works" ) {
   REQUIRE(histo.size() != 0);
   REQUIRE(histo.empty() != true);
 
+}
+
+
+TEST_CASE_METHOD(histogram_fixture<16> , "histogram_right" ) {
+
+  auto max_el = std::max_element(histo.cbegin(),histo.cend());
+
+  REQUIRE( max_el != histo.cbegin());
 }
